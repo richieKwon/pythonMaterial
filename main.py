0150408f30791a5b1b76a1ccd22ca8a8d0cddf94
richie <richie.kwon1@gmail.com>
2023-06-21 20:21:41 +0100

    class material

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 class DataProcess:
+    
+    
+    FYstart = if datetime.now().month >= 4:
+     datetime.date(datetime.now().year, datetime.now().month,1)  else 
+    FYend = 
+    FY24months
     def __init__(self, productName):
         self.product = productName
-        
+
